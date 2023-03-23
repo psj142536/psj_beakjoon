ab8de53b474aaf4d0c70fe018be5a700b439c664
psj142536 <123882358+psj142536@users.noreply.github.com>
2023-03-23 22:21:31 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	printf("%d * 1 = %d\n", n, n * 1);
+	printf("%d * 2 = %d\n", n, n * 2);
+	printf("%d * 3 = %d\n", n, n * 3);
+	printf("%d * 4 = %d\n", n, n * 4);
+	printf("%d * 5 = %d\n", n, n * 5);
+	printf("%d * 6 = %d\n", n, n * 6);
+	printf("%d * 7 = %d\n", n, n * 7);
+	printf("%d * 8 = %d\n", n, n * 8);
+	printf("%d * 9 = %d\n", n, n * 9);
+	return 0;
+}
