b307ee7cb7602ba69254474053506ae38a141c95
psj142536 <123882358+psj142536@users.noreply.github.com>
2023-03-23 22:22:10 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS    
+#include <stdio.h>
+int main()
+{
+    int a = 0;
+    scanf("%d", &a);
+    if (a >= 90)
+        printf("A");
+    else if (a >= 80 && a < 90)
+        printf("B");
+    else if (a >= 70 && a < 80)
+        printf("C");
+    else if (a >= 60 && a < 70)
+        printf("D");
+    else
+        printf("F");
+    return 0;
+}
