1cc20529a66f635d30218423e962f2c345b2e807
psj142536 <123882358+psj142536@users.noreply.github.com>
2023-09-19 14:49:50 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -0,0 +1,12 @@
+//1026_보물
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
+#include <string.h>
+int main() 
+{
+	char a[100] = { NULL, };
+	scanf("%s", a);
+	printf("%d", strlen(a));
+	return 0;
+}
