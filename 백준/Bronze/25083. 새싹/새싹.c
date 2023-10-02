1f0482c26fe0f3c9c067453e03aeb464a411913f
psj142536 <123882358+psj142536@users.noreply.github.com>
2023-10-02 12:55:24 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+int main()
+{
+	printf("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+	return 0;
+}
